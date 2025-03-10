33c2d31aac7e81a9d4dab889effe36655b262c05
Shambi <Shambi@DESKTOP-B1NJRU8>
2025-03-10 12:10:24 -0500

    Update CMake configuration and simplify source files
    - Updated CMake to require version 3.25 and defined the project "dplnk" with version 0.0.1.
    - Changed the target from executable to library, including source and header files from specified directories.
    - Set properties for interprocedural optimization and C++23 compliance, and added installation rules for the library and headers.
    - Simplified `dplnk.h` to only include a `#pragma once` directive.
    - Modified `dplnk.cpp` to include `dplnk.h`, removing all previous content.

--- src/dplnk.cpp
+++ src/dplnk.cpp
@@ -1,12 +1,1 @@
-﻿// dplnk.cpp : Defines the entry point for the application.
-//
-
-#include "dplnk.h"
-
-using namespace std;
-
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
-}
+﻿#include "dplnk.h"
